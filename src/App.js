452e07d35ae45a1bee41d1581d2d3b1972a44346
Veridiana Maricato <maricato.veridiana@gmail.com>
2023-01-16 21:00:46 -0300

    hook ready for use

--- src/App.js
+++ src/App.js
@@ -1,23 +1,95 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { useState, useEffect } from 'react';
+
+import { useFetch } from './hooks/useFetch';
+
+const url = 'http://localhost:3000/products'
+
 function App() {
+
+  const [products, setProducts] = useState([])
+
+  // custom hook
+  const { data: items, httpConfig, loading, error } = useFetch(url)
+
+  const [name, setName] = useState('')
+  const [price, setPrice] = useState('')
+
+  // resgatando os dados
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     const res = await fetch(url)
+  //     const data = await res.json()
+  //     setProducts(data)
+  //   }
+  //   fetchData()
+  // }, [])
+
+  // adicionando produtos
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const product = {
+      name,
+      price
+    }
+    console.log('oi')
+    // const res = await fetch(url, {
+    //   method: 'POST',
+    //   headers:{
+    //     'Content-Type': 'application/json'
+    //   },
+    //   body: JSON.stringify(product)
+    // })
+
+    // // carregamento dinamico
+    // const addedProduct = await res.json()
+    // setProducts((prevProducts) => [...prevProducts, addedProduct])
+
+    // refatorando produto
+    httpConfig(product, 'POST')
+    setName('')
+    setPrice('')
+  }
+
+  const handleRemove = (id) => {
+    httpConfig(id, 'DELETE')
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Lista de produtos</h1>
+
+      {/* loading  */}
+      {loading && <p>Carregando dados...</p>}
+      {error && <p>{error}</p>}
+
+      {!loading &&  <ul>
+        {items && items.map((item) => (
+          <li key={item.id}>{item.name} - R$ {item.price}
+          <button onClick={() => handleRemove(item.id)}>delete</button>
+          </li>
+          
+        ))}
+      </ul>}
+      <div className="add_product">
+        <form onSubmit={handleSubmit}>
+          <label>
+            <span>Nome:</span>
+            <input type="text" value={name} name='name' onChange={(e) => { setName(e.target.value) }} />
+          </label>
+          <label>
+            <span>Preço:</span>
+            <input type="number" value={price} price='price' onChange={(e) => { setPrice(e.target.value) }} />
+          </label>
+
+          {/* loading state on POST */}
+          {!loading && <input type="submit" value='Enviar produto' />}
+          {loading && <input type="submit" value='Enviar produto' disabled />}
+        </form>
+      </div>
     </div>
   );
 }
---
+++ src/hooks/useFetch.js
@@ -0,0 +1,96 @@
+import { useState, useEffect } from "react";
+
+//custom hooks
+export const useFetch = (url) => {
+    const [data, setData] = useState(null)
+
+
+    // refatorando post
+    const [config, setConfig] = useState(null)
+    const [method, setMethod] = useState(null)
+    const [callFetch, setCallFetch] = useState(false)
+
+    // loading
+    const [loading, setLoading] = useState(false)
+
+    // tratando erros
+    const [error, setError] = useState(null)
+  
+
+    //deletando
+    const [deleteButton, setDeleteButton] = useState(false)
+    const [itemId, setItemId] = useState(null)
+
+    const httpConfig = (data, method) => {
+        if (method === 'POST') {
+            setConfig({
+                method,
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+
+            })
+            setMethod(method)
+        } else if (method === 'DELETE') {
+            setConfig({
+                method,
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+            )
+            setMethod(method)
+            setItemId(data)
+        }
+    }
+
+    useEffect(() => {
+        const fetchData = async () => {
+
+            //loading
+            setLoading(true)
+
+            // tratanto erro
+
+            try {
+                const res = await fetch(url)
+                const json = await res.json()
+                setData(json)
+            } catch (error) {
+                console.log(error.message)
+                setError('Houve um erro ao carregar a página...')
+            }
+
+            setLoading(false)
+
+        }
+        fetchData()
+
+    }, [url, callFetch])
+
+    // refatorando post
+
+    useEffect(() => {
+
+        const httpRequest = async () => {
+            let json 
+            if (method === 'POST') {
+                let fetchOptions = [url, config]
+                const res = await fetch(...fetchOptions)
+                const json = await res.json()
+                setCallFetch(json)
+            }else if (method === 'DELETE'){
+                const deleteUrl = `${url}/${itemId}`
+                const res = await fetch(deleteUrl, config)
+
+                json = await res.json()            
+            }
+            setCallFetch(json)
+        }
+        httpRequest()
+    }, [config, method, url])
+
+
+    return { data, httpConfig, loading, error }
+}
